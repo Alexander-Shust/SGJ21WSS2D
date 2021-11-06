3dbc77ef3ce5ae0d1544dae825d41d4486047307
Alexander Shust <ashust@mail.ru>
2021-11-06 05:21:46 +0300

    First animation animates

---
+++ Assets/Scripts/Systems/CopyTransformSystem.cs
@@ -0,0 +1,21 @@
+﻿using Components;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Systems
+{
+    public class CopyTransformSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities.WithAll<CopyTransformComponent>()
+                .ForEach((Entity entity, in LocalToWorld localToWorld) =>
+                {
+                    var transform = EntityManager.GetComponentObject<Transform>(entity);
+                    transform.position = localToWorld.Position;
+                    transform.rotation = localToWorld.Rotation;
+                }).WithoutBurst().Run();
+        }
+    }
+}
