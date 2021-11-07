9de40a921abaa2d81aca7a07cfdb78248392da0f
Alexander Shust <ashust@mail.ru>
2021-11-07 10:43:52 +0300

    MailBox added

---
+++ Assets/Scripts/Systems/MailBoxFullSystem.cs
@@ -0,0 +1,20 @@
+﻿using Components;
+using Unity.Entities;
+
+namespace Systems
+{
+    public class MailBoxFullSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            RequireSingletonForUpdate<MailBoxFullComponent>();
+        }
+
+        protected override void OnUpdate()
+        {
+            EntityManager.DestroyEntity(GetSingletonEntity<MailBoxFullComponent>());
+            
+            this.ShowDialog("Работа мастера, впихнули невпихуемое! Продолжаем разговор...", 2.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/MailBoxSystem.cs
@@ -0,0 +1,57 @@
+﻿using Components;
+using Physics.Components;
+using Physics.Systems;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Transforms;
+
+namespace Systems
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateAfter(typeof(StatefulCollisionEventsSystem))]
+    public class MailBoxSystem : SystemBase
+    {
+        private EndSimulationEntityCommandBufferSystem _ecbSystem;
+        private StatefulCollisionEventsSystem _collisionSystem;
+
+        protected override void OnCreate()
+        {
+            RequireSingletonForUpdate<GameSettingsComponent>();
+            RequireSingletonForUpdate<ScoreComponent>();
+            _ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            _collisionSystem = World.GetOrCreateSystem<StatefulCollisionEventsSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var settings = GetSingleton<GameSettingsComponent>();
+            var scoreEntity = GetSingletonEntity<ScoreComponent>();
+            Dependency = JobHandle.CombineDependencies(_collisionSystem.OutDependency, Dependency);
+            var ecb = _ecbSystem.CreateCommandBuffer().AsParallelWriter();
+            Entities.WithAll<MailBoxComponent, StatefulCollisionEvent>()
+                .ForEach((Entity mailBoxEntity, int entityInQueryIndex, in Translation translation, in DynamicBuffer<StatefulCollisionEvent> events) =>
+                {
+                    foreach (var collisionEvent in events)
+                    {
+                        if (collisionEvent.CollidingState != EventCollidingState.BeginColliding)
+                            continue;
+                        var playerEntity = collisionEvent.GetOtherEntity(mailBoxEntity);
+                        var playerComponent = GetComponent<PlayerComponent>(playerEntity);
+                        if (!playerComponent.HasBox)
+                            continue;
+                        playerComponent.HasBox = false;
+                        ecb.SetComponent(entityInQueryIndex, playerEntity, playerComponent);
+                        ecb.RemoveComponent<MailBoxComponent>(entityInQueryIndex, mailBoxEntity);
+                        ecb.RemoveComponent<TrapComponent>(entityInQueryIndex, mailBoxEntity);
+                        var newEntity = ecb.CreateEntity(entityInQueryIndex);
+                        ecb.AddComponent<MailBoxFullComponent>(entityInQueryIndex, newEntity);
+                        var currentScore = GetComponent<ScoreComponent>(scoreEntity).Value;
+                        ecb.SetComponent(entityInQueryIndex, scoreEntity, new ScoreComponent {Value = currentScore + settings.TrapBonus});
+                        break;
+                    }
+                }).ScheduleParallel();
+            _ecbSystem.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
